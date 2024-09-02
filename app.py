fe14bd89e2deb551d022bd678d080f3a2b67c615
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-09-02 10:24:12 +0300

    Normalize all line endings

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
+# Access the environment variables
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
 @app.route("/")
 @app.route("/index")
